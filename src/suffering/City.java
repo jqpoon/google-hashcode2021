c99001023634364920f0ccc02b28f80fa8ed4d4c
Jia Qi Poon <jqp18@ic.ac.uk>
2021-02-26 03:21:33 +0800

    Add simulation and city

--- src/suffering/City.java
+++ src/suffering/City.java
@@ -1,16 +1,18 @@
 package suffering;
 
 import java.util.List;
+import java.util.Map;
 
 public class City {
+
   private final int simulationTime;
   private final int bonusPoints;
-  private final List<Intersection> intersections;
-  private final List<Street> streets;
+  private final Map<Integer, Intersection> intersections;
+  private final Map<String, Street> streets;
   private final List<Car> cars;
 
   public City(int simulationTime, int bonusPoints,
-      List<Intersection> intersections, List<Street> streets,
+      Map<Integer, Intersection> intersections, Map<String, Street> streets,
       List<Car> cars) {
     this.simulationTime = simulationTime;
     this.bonusPoints = bonusPoints;
