6161b4fb4286e0f06d0cd11414a0698e20fca560
Ivy Tam <iat19@ic.ac.uk>
2021-02-26 02:27:24 +0800

    [iat19] Added gitignore and populated stree class

--- src/Street.java
+++ src/Street.java
@@ -1,3 +1,29 @@
 public class Street {
+    private final String name;
+    private final int interStart;
+    private final int interEnd;
+    private final int travelTime;
 
+    public Street(String name, int interStart, int interEnd, int travelTime) {
+        this.name = name;
+        this.interStart = interStart;
+        this.interEnd = interEnd;
+        this.travelTime = travelTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getInterStart() {
+        return interStart;
+    }
+
+    public int getInterEnd() {
+        return interEnd;
+    }
+
+    public int getTravelTime() {
+        return travelTime;
+    }
 }
