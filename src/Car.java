04d5d53c32856ea2b07e42bb6b1fc41bc43f2ef2
Zhai Zirun <zz4319@ic.ac.uk>
2021-02-26 02:27:59 +0800

    populated Car class

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,40 @@
+import java.util.List;
+
 public class Car {
 
+  private int streetCount;
+  private final List<String> streetNames;
+  private int currentStreetNum;
+  private String currentStreet;
+
+  public Car(int streetCount, List<String> streetNames) {
+    this.streetCount = streetCount;
+    this.streetNames = streetNames;
+    currentStreet = streetNames.get(0);
+  }
+
+  public int getStreetCount() {
+    return streetCount;
+  }
+
+  public void setStreetCount(int streetCount) {
+    this.streetCount = streetCount;
+  }
+
+  public String getCurrentStreet() {
+    return currentStreet;
+  }
+
+  public void setCurrentStreet(int currentStreetNum) {
+    this.currentStreet = streetNames.get(currentStreetNum);
+    this.currentStreetNum = currentStreetNum;
+  }
+
+  public void advance() {
+    setCurrentStreet(currentStreetNum + 1);
+  }
+
+  public List<String> getStreetNames() {
+    return streetNames;
+  }
 }
