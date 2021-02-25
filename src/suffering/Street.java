9054d340f7e869a2e90fcd140fd4ae7b9b6a383a
Zhai Zirun <zz4319@ic.ac.uk>
2021-02-26 03:51:55 +0800

    changed cars in street

--- src/suffering/Street.java
+++ src/suffering/Street.java
@@ -5,25 +5,26 @@ import java.util.List;
 import suffering.TrafficLight.Color;
 
 public class Street {
+    private final City city;
     private final String name;
     private final int interStart;
     private final int interEnd;
     private final int travelTime;
     private final TrafficLight lightStart;
     private final TrafficLight lightEnd;
-    private List<Car> carsAtEnd;
-    private List<Car> carsAtStart;
+
+    private List<Car> cars;
 
 
-    public Street(String name, int interStart, int interEnd, int travelTime) {
+    public Street(City city, String name, int interStart, int interEnd, int travelTime) {
+        this.city = city;
         this.name = name;
         this.interStart = interStart;
         this.interEnd = interEnd;
         this.travelTime = travelTime;
         this.lightStart = new TrafficLight(Color.RED, this, false);
         this.lightEnd = new TrafficLight(Color.RED, this, true);
-        carsAtEnd = new ArrayList<>();
-        carsAtStart = new ArrayList<>();
+        cars = new ArrayList<>();
     }
 
     public String getName() {
@@ -50,11 +51,8 @@ public class Street {
         return lightEnd;
     }
 
-    public List<Car> getCarsAtEnd() {
-        return carsAtEnd;
+    public List<Car> getCars() {
+        return cars;
     }
 
-    public List<Car> getCarsAtStart() {
-        return carsAtStart;
-    }
 }
